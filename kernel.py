74794478ba3fbfe945f321cee4ec5a0a0676439f
paters06 <paters15@gmail.com>
2022-09-13 23:19:32 -0500

    Homogeneous heat transfer. Code not fully debugged

--- kernel.py
+++ kernel.py
@@ -1,8 +1,10 @@
 import math
 import numpy as np
 
+# class KernelFunction:
 
-def cubic_spline(x: np.ndarray, x_prime: np.ndarray, h: float) -> float:
+
+def cubic_spline_kernel(x: np.ndarray, x_prime: np.ndarray, h: float) -> float:
     """
     Compute the kernel function through the cubic spline formula
 
@@ -15,12 +17,44 @@ def cubic_spline(x: np.ndarray, x_prime: np.ndarray, h: float) -> float:
         - h (`float'): kernel function radius
     """
     q = np.linalg.norm(x - x_prime)/h
+    # print(q)
     alpha_D = 15/(7*math.pi*h**2)
 
     if q >= 0.0 and q <= 1.0:
         return alpha_D*((2/3) - q**2 + 0.5*q**3)
     elif q > 1.0 and q <= 2.0:
-        return alpha_D*((1/6)*(2 - q**3))
+        return alpha_D*((1/6)*(2 - q)**3)
+    else:
+        return 0
+
+
+def cubic_spline_kernel_derivative(x: np.ndarray, x_prime: np.ndarray,
+                                   h: float):
+    q = np.linalg.norm(x - x_prime)/h
+    
+    alpha_D = 15/(7*math.pi*h**2)
+    # print("q: {:.3f}".format(q))
+    # print("h: {:.3f}".format(h))
+    # print("a_D: {:.3f}".format(alpha_D))
+
+    if q >= 0.0 and q <= 1.0:
+        # print("a_D: {:.3f}".format(alpha_D))
+        # print("x:", x)
+        # print("x':", x_prime)
+        # print(np.linalg.norm(x-x_prime))
+        # print(np.sqrt((x[0] - x_prime[0])**2 + (x[1] - x_prime[1])**2))
+        # print("q: {:.3f}".format(q))
+        # print("Polynomial: {:.3f}".format(-2*q + 1.5*q**2))
+        return alpha_D*(-2*q + 1.5*q**2)
+    elif q > 1.0 and q <= 2.0:
+        # print("a_D: {:.3f}".format(alpha_D))
+        # print("x:", x)
+        # print("x':", x_prime)
+        # print(np.linalg.norm(x-x_prime))
+        # print(np.sqrt((x[0] - x_prime[0])**2 + (x[1] - x_prime[1])**2))
+        # print("q: {:.3f}".format(q))
+        # print("Polynomial: {:.3f}".format((-(2 - q)**2)/2))
+        return alpha_D*(-(2 - q)**2)/2
     else:
         return 0
 
@@ -46,6 +80,16 @@ def lucy_quartic_kernel(x: np.ndarray, x_prime: np.ndarray, h: float) -> float:
         return 0
 
 
+def lucy_quartic_kernel_derivative(x: np.ndarray, x_prime: np.ndarray,
+                                   h: float) -> float:
+    q = np.linalg.norm(x - x_prime)/h
+    alpha_D = 5/(math.pi*h**2)
+    if q >= 0.0 and q <= 1.0:
+        return alpha_D*(-12*q + 24*q**2 - 12*q**3)
+    else:
+        return 0
+
+
 def new_quartic_kernel(x: np.ndarray, x_prime: np.ndarray, h: float) -> float:
     """
     Compute the kernel function through the cubic spline formula
@@ -63,6 +107,17 @@ def new_quartic_kernel(x: np.ndarray, x_prime: np.ndarray, h: float) -> float:
 
     if q >= 0.0 and q <= 2.0:
         return alpha_D*((2/3) - (9/8)*q**2 + (19/24)*q**3 - (5/32)*q**4)
+    else:
+        return 0
+
+
+def new_quartic_kernel_derivative(x: np.ndarray, x_prime: np.ndarray,
+                                  h: float) -> float:
+    q = np.linalg.norm(x - x_prime)/h
+    alpha_D = 15/(7*math.pi*h**2)
+
+    if q >= 0.0 and q <= 2.0:
+        return alpha_D*(- (18/8)*q + (57/24)*q**2 - (20/32)*q**3)
     else:
         return 0
 
@@ -88,5 +143,21 @@ def quintic_kernel(x: np.ndarray, x_prime: np.ndarray, h: float) -> float:
         return alpha_D*((3 - q)**5 - 6*(2 - q)**5)
     elif q > 2.0 and q <= 3.0:
         return alpha_D*((3 - q)**5)
+    else:
+        return 0
+
+
+def quintic_kernel_derivative(x: np.ndarray, x_prime: np.ndarray,
+                              h: float) -> float:
+
+    q = np.linalg.norm(x - x_prime)/h
+    alpha_D = 7/(478*math.pi*h**2)
+
+    if q >= 0.0 and q <= 1.0:
+        return alpha_D*(-120*q + 120*q**3 - 50*q**4)
+    elif q > 1.0 and q <= 2.0:
+        return alpha_D*(75 - 420*q + 450*q**2 - 180*q**3 + 25*q**4)
+    elif q > 2.0 and q <= 3.0:
+        return alpha_D*(405 + 540*q - 270*q**2 + 60*q**3 - 5*q**4)
     else:
         return 0
--- main.py
+++ main.py
@@ -1,6 +1,204 @@
+import math
+import numpy as np
+import kernel as kn
+from matplotlib import pyplot as plt
+
+
+def particle_creation(lx, ly, npx, npy):
+    x = np.linspace(0, lx, npx)
+    y = np.linspace(0, ly, npy)
+
+    np_2d = npx*npy
+
+    x_field, y_field = np.meshgrid(x, y)
+    x_field_1d = np.reshape(x_field, (1, np_2d))
+    y_field_1d = np.reshape(y_field, (1, np_2d))
+
+    particles = np.vstack((x_field_1d, y_field_1d))
+
+    return particles
+
+
+def neighbor_list(i, particle_list, influence_radius):
+    neigh_idx = []
+    num_particle = particle_list.shape[1]
+    X_i = particle_list[:,i]
+    for j in range(0, num_particle):
+        X_j = particle_list[:,j]
+        d = np.linalg.norm(X_i - X_j)
+        if d < influence_radius:
+            neigh_idx.append(j)
+
+    return particle_list[:,neigh_idx]
+
+
+def particle_density(i, h, particle_list, m_list, influence_radius):
+    rho_i = 0
+    X_i = particle_list[:,i]
+    num_particles = particle_list.shape[1]
+    neighbor_particle = neighbor_list(i, particle_list, influence_radius)
+    num_neighbors = neighbor_particle.shape[0]
+
+    for j in range(0, num_neighbors):
+        X_j = neighbor_particle[:,j]
+        # print(kn.cubic_spline_kernel(X_i, X_j, h))
+        rho_i += m_list[:,j]*kn.cubic_spline_kernel(X_i, X_j, h)
+
+    # print(rho_i)
+    return rho_i
+
+
+def discretized_laplacian(K, rho, cv, dx, dy, m_list, rho_list,
+                          particle_list, T_field, i):
+    
+    alpha_i = K/(rho*cv)
+
+    influence_radius = 2.5*dx
+    k = 2.0
+    h = influence_radius/k
+
+    num_particles = m_list.shape[1]
+    X_i = particle_list[:,i]
+
+    particle = 0
+
+    for j in range(1, num_particles):
+        # rho_j = particle_density(j, h, particle_list, m_list, influence_radius)
+        X_j = particle_list[:,j]
+        particle += ((m_list[:,j]/rho_list[:,j])*(T_field[:,i] - T_field[:,j])
+                      * kn.cubic_spline_kernel_derivative(X_i, X_j, h))
+        # print((m_list[:,j]/rho_list[:,j])*(T_field[:,i] - T_field[:,j])
+        #                * kn.cubic_spline_kernel_derivative(X_i, X_j, h))
+
+    # print(particle)
+    laplace_i = 2*alpha_i*particle
+
+    return laplace_i
+
+
+def initial_conditions(lx, ly, npx, npy, T_0y, T_1y, T_x0, T_x1, T_xy):
+    x = np.linspace(0, lx, npx)
+    y = np.linspace(0, ly, npy)
+
+    np_2d = npx*npy
+
+    x_field, y_field = np.meshgrid(x, y)
+    x_field_1d = np.reshape(x_field, (1, np_2d))
+    y_field_1d = np.reshape(y_field, (1, np_2d))
+
+    idx_0 = np.where(x_field_1d[0,:,None] == 0.0)[0]
+    idx_1 = np.where(x_field_1d[0,:,None] == 1.0)[0]
+
+    idy_0 = np.where(y_field_1d[0,:,None] == 0.0)[0]
+    idy_1 = np.where(y_field_1d[0,:,None] == 1.0)[0]
+
+    T_field = np.full((1, np_2d), T_xy)
+    T_field[:,idx_0] = T_0y
+    T_field[:,idx_1] = T_1y
+    T_field[:,idy_0] = T_x0
+    T_field[:,idy_1] = T_x1
+
+    # print(idx_0)
+    # print(idx_1)
+
+    # print(idy_0)
+    # print(idy_1)
+
+    return T_field
+
+
+def analytical_solution(particle_list, Ts):
+    num_series_iter = 10
+    num_particles = particle_list.shape[1]
+    N = num_particles
+    T_exact = np.zeros((1, num_particles))
+    delta_T = np.zeros((1, num_particles))
+
+    for i in range(0,num_particles):
+        for N in range(1,num_series_iter):
+            AN = ((2*Ts)/(N*math.pi))*(((-1)**N - 1)/(np.sinh(N*math.pi)))
+            delta_T[0,i] = AN*np.sin(N*math.pi*particle_list[0,i])\
+                * np.sinh(N*math.pi*(particle_list[1,i] - 1))
+            np.add(T_exact, delta_T, out=T_exact, casting="unsafe")
+
+    return T_exact
+
+
+def numerical_error(T_numerical, T_analytical):
+    return (T_analytical - T_numerical)
+
 
 def main():
-    print('First source files')
+    print('Preprocessing')
+    num_particles_x = 20
+    num_particles_y = 20
+    # num_particles = num_particles_x*num_particles_y
+
+    Lx = 1.0
+    Ly = 1.0
+
+    dx = Lx/num_particles_x
+    dy = Ly/num_particles_y
+
+    K = 1
+    rho = 1
+    cv = 1
+
+    Ts = 100
+
+    T_0y = 0
+    T_1y = 0
+    T_x0 = Ts
+    T_x1 = 0
+    T_xy = 0
+
+    dt = 1e-1
+    t_final = 1
+    time_steps = np.arange(0, t_final, step=dt)
+    num_steps = len(time_steps)
+
+    particle_list = particle_creation(Lx, Ly, num_particles_x, num_particles_y)
+    num_particles = particle_list.shape[1]
+    particle_densities = np.ones((1, num_particles))
+    particle_masses = dx*dy*particle_densities
+    T_field = initial_conditions(Lx, Ly, num_particles_x, num_particles_y,
+                                 T_0y, T_1y, T_x0, T_x1, T_xy)
+
+    # print(T_field)
+    print('Solver')
+    # num_particles = 1
+    laplacian = np.zeros((1, num_particles))
+    # print(laplacian)
+
+    for i_time in range(0, num_steps):
+        print("Time iteration #{:d}".format(i_time+1))
+        for i in range(0, num_particles):
+            # print("Particle #{:d}".format(i+1))
+            laplacian[0,i] = discretized_laplacian(K, rho, cv, dx, dy,
+                                                 particle_masses,
+                                                 particle_densities,
+                                                 particle_list, T_field, i)
+            # print(laplacian[0,i])
+        # print(laplacian)
+        # print(dt)
+        # T_field += (laplacian*dt)
+        np.add(T_field, laplacian*dt, out=T_field, casting="unsafe")
+
+    print('Postprocessing')
+    # print(T_field)
+
+    T_exact = analytical_solution(particle_list, Ts)
+    # print(T_exact)
+
+    interpolation_error = numerical_error(T_field, T_exact)
+    print('Maximum temperature difference: {:.3f} °C'
+          .format(np.max(interpolation_error)))
+
+    plt.scatter(particle_list[0,:], particle_list[1,:], c=T_exact)
+    plt.figure()
+    plt.scatter(particle_list[0,:], particle_list[1,:], c=T_field)
+    plt.show()
+
 
 
 if __name__ == "__main__":
